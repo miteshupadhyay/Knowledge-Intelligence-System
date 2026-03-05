5964ae707ef53ca480cd08a317a71f0dc4600224
Mitesh Upadhyay <upadhyaymitesh91@gmail.com>
2026-03-05 14:17:22 +0530

    First Commit

--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -6,8 +6,8 @@ class S3Storage:
     def __init__(self):
         self.s3_client = boto3.client(
             's3',
-            aws_access_key_id="AKIA35XJGFRA7RG2XGWH",
-            aws_secret_access_key="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",
+            aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,
             region_name="us-east-1"
         )
         self.bucket_name = Config.S3_BUCKET_NAME
